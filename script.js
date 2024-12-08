76512465537edc8c7af7146c2564d9729ce0e8b1
MobyKsiu <ksenya.may2008@gmail.com>
2024-12-08 20:53:28 +0100

    ver.0.2.beta
    Kalkulator 1

---
+++ script.js
@@ -0,0 +1,6 @@
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input');
+const display_output = document.querySelector('.display .output');
+
+let input = "";
+
