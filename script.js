604e74cb424a11e4cb668c53a5f38c4efa1c6eb5
MobyKsiu <ksenya.may2008@gmail.com>
2024-12-08 20:59:09 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const keys = document.querySelectorAll('.key');
-const display_input = document.querySelector('.display .input');
-const display_output = document.querySelector('.display .output');
-
-let input = "";
-
